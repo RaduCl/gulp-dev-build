394d1c078a82e1e41dc071fc9c6e7dcda5f498e1
RaduCl <raduclitan@gmail.com>
2016-04-26 16:04:36 +0300

    implmented side-bar toggle

--- deploy/js/main.js
+++ deploy/js/main.js
@@ -1,9 +1,23 @@
 $(document).ready(function() {
-  initBurgerMenu();
+  toggleBurgerMenu();
+  initSideNavToggle();
 });
 
-function initBurgerMenu() {
+function toggleBurgerMenu() {
   $('#side-nav-toggle').on('click', function(){
     $(this).toggleClass('active');
+    $('#sidebar-menu').toggleClass('closed')
+    if($('#collapseOne').hasClass('in')) $('#collapseOne').toggleClass('in');
+    if($('#collapseTwo').hasClass('in')) $('#collapseTwo').toggleClass('in');
   });
+}
+
+function initSideNavToggle() {
+  $('.collection-toggle').on('click', function(){
+    if($('#sidebar-menu').hasClass('closed')){
+      $('#sidebar-menu').toggleClass('closed');
+      $('#side-nav-toggle').toggleClass('active');
+    }
+  });
+
 }
